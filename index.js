9de03d0785da9cfc5e406edb2d9956afc11f2478
Fulvio Salza <fsalza96@gmail.com>
2023-04-20 10:59:44 -0400

    created first function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(array){
+    let record;
+    return record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord)
+}
